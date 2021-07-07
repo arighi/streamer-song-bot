c9c54d1d76d8b5489a6aa52ca0678a45feed50b7
Andrea Righi <andrea.righi@canonical.com>
2021-07-07 10:45:39 +0200

    update font and better exception handling
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- now-playing.py
+++ now-playing.py
@@ -15,33 +15,36 @@ template = """
   <meta http-equiv="refresh" content="5">
 </head>
 <body>
-<p style="color:white;font-size:22px">%s</p>
+<p style="color:white;font-size:26px">%s</p>
 </body>
 </html>
 """
 
 def main():
     while True:
-        r = requests.get(song_queue)
-        data = json.loads(r.text)
         try:
-            user = data["list"][0]["requests"][0]["name"]
-            song = data["list"][0]["song"]
-            if song is None:
-                song = data["list"][0]["nonlistSong"]
-                text = 'Live-learn: "<i>%s</i>" requested by <b>%s</b>' % \
-                       (song, user)
-            else:
-                title = song["title"]
-                artist = song["artist"]
-                text = 'Now playing: "<i>%s - %s</i>" requested by <b>%s</b>' % \
-                       (song["title"], song["artist"], user)
-        except:
-            text = "No song is being played (just chatting/practicing...)"
-        with open(CWD + '.index.html', 'w') as fd:
-            fd.write(template % text)
-        os.rename(CWD + '.index.html', CWD + 'index.html')
-        print(text)
+            r = requests.get(song_queue)
+            data = json.loads(r.text)
+            try:
+                user = data["list"][0]["requests"][0]["name"]
+                song = data["list"][0]["song"]
+                if song is None:
+                    song = data["list"][0]["nonlistSong"]
+                    text = 'Live-learn: "<i>%s</i>" requested by <b>%s</b>' % \
+                           (song, user)
+                else:
+                    title = song["title"]
+                    artist = song["artist"]
+                    text = 'Now playing: "<i>%s - %s</i>" requested by <b>%s</b>' % \
+                           (song["title"], song["artist"], user)
+            except:
+                text = "No piece is being played (just chatting...)"
+            with open(CWD + '.index.html', 'w') as fd:
+                fd.write(template % text)
+            os.rename(CWD + '.index.html', CWD + 'index.html')
+            print(text)
+        except Exception as e:
+            print(e)
         sleep(10)
 
 if __name__ == '__main__':
